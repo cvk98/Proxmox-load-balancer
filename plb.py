e55e6451906d381700ae145c48026cbd83a731f7
Rinat Khaliullin <rinateg@gmail.com>
2022-05-20 20:59:35 +0300

    Minor improvements suggested by Cylindric

--- plb.py
+++ plb.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-# Proxmox-load-balancer v0.5.1-betta Copyright (C) 2022 cvk98 (github.com/cvk98)
+# Proxmox-load-balancer v0.5.2-betta Copyright (C) 2022 cvk98 (github.com/cvk98)
 
 import sys
 import requests
